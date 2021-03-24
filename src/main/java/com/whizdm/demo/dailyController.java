dac7b9778188749b4e68892437f6c1aadcfa1adc
Vipin R Bharadwaj <vipin.bharadwaj@moneyview.in>
2021-03-24 16:37:18 +0530

    Db

--- src/main/java/com/whizdm/demo/dailyController.java
+++ src/main/java/com/whizdm/demo/dailyController.java
@@ -1,10 +1,7 @@
 package com.whizdm.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,11 +13,13 @@ public class dailyController {
     daily_repository repod;
 
     @PostMapping("/daily/populate")
+    @CrossOrigin
     public Optional popu(@RequestBody daily_table dai){
         repod.save(dai);
         return repod.findById(dai.getSl_no());
     }
     @GetMapping("/daily/getfulldata")
+    @CrossOrigin
     public List<daily_table> queryTable(){
         List<daily_table> m = repod.findfulldata();
         System.out.println(m.toString());
@@ -29,6 +28,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getpoddata")
+    @CrossOrigin
     public List<Object> querypod(){
         List<Object> l = repod.findpod();
         System.out.println(l.toString());
@@ -36,6 +36,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getimpactdata")
+    @CrossOrigin
     public List<Object> queryimpact(){
         List<Object> l = repod.findimpact();
         System.out.println(l.toString());
@@ -43,6 +44,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getimpactareasdata")
+    @CrossOrigin
     public List<Object> queryimpactarea(){
         List<Object> l = repod.findimpactareas();
         System.out.println(l.toString());
@@ -50,6 +52,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getrelease_typedata")
+    @CrossOrigin
     public List<Object> queryrelease_type(){
         List<Object> l = repod.findrelease_type();
         System.out.println(l.toString());
@@ -57,6 +60,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getgenericdata")
+    @CrossOrigin
     public List<Object> querygeneric(){
         List<Object> l = repod.findgenerics();
         System.out.println(l.toString());
@@ -64,6 +68,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getbranch_namedata")
+    @CrossOrigin
     public List<Object> querybranch_name(){
         List<Object> l = repod.findbranch_name();
         System.out.println(l.toString());
@@ -71,6 +76,7 @@ public class dailyController {
     }
 
     @GetMapping("/daily/getsqlqueriesapproval")
+    @CrossOrigin
     public List<Object> querysql_queries_approval(){
         List<Object> l = repod.findsql_queries_approval();
         System.out.println(l.toString());
