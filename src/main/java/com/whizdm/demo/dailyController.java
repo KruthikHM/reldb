3eaf8d56ab381ae0b2599334e38cfb8237b14a4d
hmkruthik <kruthik.hm@moneyview.in>
2021-03-24 12:49:29 +0530

    Added controller and repo for all the classes

--- src/main/java/com/whizdm/demo/dailyController.java
+++ src/main/java/com/whizdm/demo/dailyController.java
@@ -1,10 +1,12 @@
 package com.whizdm.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,5 +20,21 @@ public class dailyController {
         repod.save(dai);
         return repod.findById(dai.getSl_no());
     }
+    @GetMapping("/daily/getfulldata")
+    public List<daily_table> queryTable(){
+        List<daily_table> m = repod.findfulldata();
+        System.out.println(m.toString());
+        return m;
+
+    }
+
+    @GetMapping("/daily/getpoddata")
+    public List<Object> querypod(){
+        List<Object> l = repod.findpod();
+        System.out.println("Hello guys");
+        System.out.println(l.toString());
+        return l;
+    }
+
 
 }
--- src/main/java/com/whizdm/demo/daily_repository.java
+++ src/main/java/com/whizdm/demo/daily_repository.java
@@ -1,6 +1,16 @@
 package com.whizdm.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface daily_repository extends JpaRepository<daily_table,Long> {
+    @Query(value = "SELECT * FROM daily_table " , nativeQuery = true)
+    List<daily_table> findfulldata();
+
+    @Query(value = "SELECT pod FROM daily_table",nativeQuery = true)
+    List<Object> findpod();
+
+
 }
